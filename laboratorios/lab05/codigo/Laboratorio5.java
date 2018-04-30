b31b4e8c523aba481aa4ebe58789ee65c316c1e5
Luis Javier Palacio Mesa <ljpalaciom@eafit.edu.co>
2018-04-30 00:39:53 -0500

    Subiendo  heldKarp
    Hermoso ese algoritmo

--- laboratorios/lab05/codigo/Laboratorio5.java
+++ laboratorios/lab05/codigo/Laboratorio5.java
@@ -6,7 +6,82 @@ package Lab5;
  * Santiago Castrillón Galvis
  */
 public class Laboratorio5 {
-   
+  
+ /**
+ * Una implementacion del algoritmo de heldKarp que permite resolver de manera optima el problema de tsp
+ * @param g El grafo a analizar
+ * @return El costo minimo del tsp
+ */
+    public static int heldKarp(Digraph g) {
+        int n = g.size();
+        int numSub = (int) Math.pow(2, n - 1);
+        int pesos[][] = new int[n][numSub];
+        LinkedList<BitmaskSet> subconjuntos = new LinkedList<>();
+
+        combinations(n - 1, subconjuntos, new ArrayList());
+
+        for (BitmaskSet sub1 : subconjuntos) {
+            for (int i = n - 1; i >= 0; --i) {
+                int min = Integer.MAX_VALUE;
+                int porDonde = 1;
+                for (BitmaskSet sub2 : subconjuntos) {
+                    if (sub1.length() <= sub2.length()) {
+                        continue;
+                    }
+                    int acum = 0;
+                    for (int k = 1; k < n; k++) {
+
+                        if (sub1.contains(k) && sub2.contains(k)) {
+                            acum++;
+                        } else if (sub1.contains(k)) {
+                            porDonde = k;
+                        }
+                    }
+                    if (acum == sub1.length() - 1) {
+                        int valor = pesos[porDonde][sub2.mask() / 2] + g.getWeight(porDonde, i);
+
+                        if (valor < min) {
+                            min = valor;
+                        }
+                    }
+                }
+                if (min == Integer.MAX_VALUE) {
+                    pesos[i][sub1.mask() / 2] = g.getWeight(0, i);
+                } else {
+                    pesos[i][sub1.mask() / 2] = min;
+                }
+            }
+        }
+        return pesos[0][numSub - 1];
+    }
+    /**
+     * Un algoritmo que obtiene un conjunto potencia de n P(A) donde A ={1,2,3...n} 
+     * @param n Es el último elemento del conjunto A
+     * @param subconjuntos En esta lista enlazada se ira agregando en orden de tamano los subconjuntos
+     * @param actual Es una variable que permite moldear cada subconjunto
+     */
+    public static void combinations(int n, LinkedList<BitmaskSet> subconjuntos, ArrayList<Integer> actual) {
+        if (n == 0) {
+            BitmaskSet temp = new BitmaskSet();
+            int size = 0;
+            for (Integer integer : actual) {
+                temp.add(integer);
+            }
+            int pos = 0;
+            for (; pos < subconjuntos.size(); pos++) {
+                if (actual.size() < (int) subconjuntos.get(pos).length()) {
+                    break;
+                }
+            }
+            subconjuntos.add(pos, temp);
+        } else {
+            actual.add(n);
+            combinations(n - 1, subconjuntos, actual);
+            actual.remove(actual.size() - 1);
+            combinations(n - 1, subconjuntos, actual);
+        }
+    }
+	
     /**
     * Método principal de la clase
     * @param args argumento predeterminado de la clase main

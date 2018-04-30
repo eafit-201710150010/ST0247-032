3372b72a10fae4278458e13b40143a21413b8753
Luis Javier Palacio Mesa <ljpalaciom@eafit.edu.co>
2018-04-30 00:18:14 -0500

    SUBIENDO CON  heldKarp
    Si señores, el algoritmo más difícil que me he tirado, no parce. Ya con esto soy el presidente del mundo

--- laboratorios/lab05/ejercicioEnLinea/EjercicioEnLinea.java
+++ laboratorios/lab05/ejercicioEnLinea/EjercicioEnLinea.java
@@ -2,6 +2,8 @@ package EjercicioEnLinea;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import lab5.*;
 
 /**
@@ -9,51 +11,81 @@ import lab5.*;
  * @author ljpalaciom
  */
 public class EjercicioEnLinea {
+
     static int menor;
-   
-    public static int recorrido(Digraph g) {
-        int arr[] = new int[g.size()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        menor = peso(g, arr);
-        recorrido(g, arr, 0);
-        return menor;
-    }
 
-    private static void cambiar(int[] arr, int i, int k) {
-        int t = arr[i];
-        arr[i] = arr[k];
-        arr[k] = t;
-    }
+    public static int heldKarp(Digraph g) {
+        int n = g.size();
+        int numSub = (int) Math.pow(2, n - 1);
+        int pesos[][] = new int[n][numSub];
+        LinkedList<BitmaskSet> subconjuntos = new LinkedList<>();
 
-    private static int peso(Digraph g, int[] arr) {
-        int acum = 0;
-        for (int i = 0; i < g.size() - 1; i++) {
-            acum += g.getWeight(arr[i], arr[i + 1]);
-        }
-        return acum += g.getWeight(arr[arr.length - 1], arr[0]);
-    }
+        combinations(n - 1, subconjuntos, new ArrayList());
 
-     private static void recorrido(Digraph g, int[] arr, int start) {
-        for (int i = start; i < arr.length; i++) {
-            cambiar(arr, i, start);
-            int peso = peso(g, arr);
-            if (peso < menor) {
-                menor = peso;
+        for (BitmaskSet sub1 : subconjuntos) {
+            for (int i = n - 1; i >= 0; --i) {
+                int min = Integer.MAX_VALUE;
+                int porDonde = 1;
+                for (BitmaskSet sub2 : subconjuntos) {
+                    if (sub1.length() <= sub2.length()) {
+                        continue;
+                    }
+                    int acum = 0;
+                    for (int k = 1; k < n; k++) {
+
+                        if (sub1.contains(k) && sub2.contains(k)) {
+                            acum++;
+                        } else if (sub1.contains(k)) {
+                            porDonde = k;
+                        }
+                    }
+                    if (acum == sub1.length() - 1) {
+                        int valor = pesos[porDonde][sub2.mask() / 2] + g.getWeight(porDonde, i);
+
+                        if (valor < min) {
+                            min = valor;
+                        }
+                    }
+                }
+                if (min == Integer.MAX_VALUE) {
+                    pesos[i][sub1.mask() / 2] = g.getWeight(0, i);
+                } else {
+                    pesos[i][sub1.mask() / 2] = min;
+                }
             }
-            recorrido(g, arr, start + 1);
-            cambiar(arr, start, i);
+        }
+        return pesos[0][numSub - 1];
+    }
+
+    public static void combinations(int n, LinkedList<BitmaskSet> subconjuntos, ArrayList<Integer> actual) {
+        if (n == 0) {
+            BitmaskSet temp = new BitmaskSet();
+            int size = 0;
+            for (Integer integer : actual) {
+                temp.add(integer);
+            }
+            int pos = 0;
+            for (; pos < subconjuntos.size(); pos++) {
+                if (actual.size() < (int) subconjuntos.get(pos).length()) {
+                    break;
+                }
+            }
+            subconjuntos.add(pos, temp);
+        } else {
+            actual.add(n);
+            combinations(n - 1, subconjuntos, actual);
+            actual.remove(actual.size() - 1);
+            combinations(n - 1, subconjuntos, actual);
         }
     }
 
     public static void main(String[] args) {
         try {
-        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-        String linea;
-        String lineaPartida[];
-        Pair<Integer, Integer>[] coordenadas;
-        
+            BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+            String linea;
+            String lineaPartida[];
+            Pair<Integer, Integer>[] coordenadas;
+
             linea = lector.readLine();
 
             int t = Integer.parseInt(linea);
@@ -84,7 +116,7 @@ public class EjercicioEnLinea {
                         );
                     }
                 }
-                System.out.println("The shortest path has length " + recorrido(grafo));
+                System.out.println("The shortest path has length " + heldKarp(grafo));
             }
         } catch (Exception e) {
         }
